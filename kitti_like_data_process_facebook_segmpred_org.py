ba089164d154c15fc53f46f3874c135d32730649
guanfuchen <guanfuchen@zju.edu.cn>
2018-01-29 11:04:02 +0800

    update

--- kitti_like_data_process_facebook_segmpred_org.py
+++ kitti_like_data_process_facebook_segmpred_org.py
@@ -7,7 +7,7 @@ from scipy import misc
 import hickle as hkl
 
 if __name__ == '__main__':
-    data_prefix = 'facebook_segmpred_org_'
+    data_prefix = 'gd_yu_'
     sources_train = []
     X_train = []
     sources_val = []
@@ -15,7 +15,8 @@ if __name__ == '__main__':
     # 间隔一定时间进行预测，防止间隔太少相似性过大
     frame_interval = 3
     HOME_PATH = os.path.expanduser('~')
-    valid_dirs = glob.glob(os.path.join(HOME_PATH, 'GitHub/Quick/HPEC/可行区域识别/工大拍摄原始数据/DJI_000[0-9]_160x128'))
+    # valid_dirs = glob.glob(os.path.join(HOME_PATH, 'GitHub/Quick/HPEC/可行区域识别/工大拍摄原始数据/DJI_000[0-9]_160x128'))
+    valid_dirs = glob.glob(os.path.join(HOME_PATH, 'Data/facebook_segmpred_Data/train_batch_*_org'))
     train_rate = 0.7
     val_rate = 1 - train_rate
     # print(valid_dirs)
@@ -24,7 +25,7 @@ if __name__ == '__main__':
         valid_dir_name = valid_dir[valid_dir.rfind('/')+1:]
         print(valid_dir)
         print(valid_dir_name)
-        valid_image_files = glob.glob(os.path.join(valid_dir, '*.bmp'))
+        valid_image_files = glob.glob(os.path.join(valid_dir, '*.png'))
         valid_image_files.sort()
         valid_image_files_len = len(valid_image_files)
 
